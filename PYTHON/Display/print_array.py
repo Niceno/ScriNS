8f305d5ebaf65106f0faa05ea522a9c80f5e1957
Bojan Niceno <bojan.niceno@psi.ch>
2017-03-25 19:08:37 +0100

    Good stuff - format can now be sent as additional parameter.

--- PYTHON/Display/print_array.py
+++ PYTHON/Display/print_array.py
@@ -1,10 +1,17 @@
 #==========================================================================
-def print_array(a):
+def print_array(*args):
 #--------------------------------------------------------------------------
+
+  if len((args)) == 1:
+    a      = args[0]
+    format = "%7.2f"
+  else:  
+    a      = args[0]
+    format = args[1]
 
   print('Array ['+('%d' %a.shape[0])+']')
   rows = a.shape[0]
   for i in range(0,rows):
-    print('%15.8e ' %a[i])
+    print(format %a[i])
 
   return  # end of function
--- PYTHON/Display/print_matrix.py
+++ PYTHON/Display/print_matrix.py
@@ -1,6 +1,13 @@
 #==========================================================================
-def print_matrix(a):
+def print_matrix(*args):
 #--------------------------------------------------------------------------
+
+  if len((args)) == 1:
+    a      = args[0]
+    format = "%7.2f"
+  else:  
+    a      = args[0]
+    format = args[1]
 
   print('Matrix['+('%d' %a.shape[0])+']['+('%d' %a.shape[1])+']')
   rows = a.shape[0]
@@ -8,10 +15,7 @@ def print_matrix(a):
 
   for i in range(0,rows):
     for j in range(0,cols):
-      if a[i,j] == 0:
-        print('    .  ', end='')
-      else:
-        print(('%7.2f' %a[i,j]), end='')
+      print((format %a[i,j]), end='')
     print('')
   print('')      
 

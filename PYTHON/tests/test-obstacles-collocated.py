c21976daee897a3b7f97f46c6e22b05170edf4b8
Mike Mueller <mmueller@python-academy.de>
2017-05-19 20:56:33 +0200

    Remove unneeded imports

--- PYTHON/tests/test-obstacles-collocated.py
+++ PYTHON/tests/test-obstacles-collocated.py
@@ -28,22 +28,21 @@
 """
 
 
-from numpy import array, zeros
+from numpy import zeros
 
 from scrins.physical_models.properties_for_air import properties_for_air
 from scrins.constants.boundary_conditions import DIRICHLET, NEUMANN, OUTLET
 from scrins.constants.coordinates import X, Y, Z
-from scrins.constants.gravitational_constant import G
 from scrins.constants.compass import W, E, S, N, B, T, C
 from scrins.display.plot_isolines import plot_isolines
 from scrins.discretization.adj_n_bnds import adj_n_bnds
 from scrins.discretization.cartesian_grid import cartesian_grid
-from scrins.discretization.nodes import nodes
 from scrins.discretization.create_unknown import create_unknown
 from scrins.discretization.cfl_max import cfl_max
 from scrins.discretization.calc_p import calc_p
 from scrins.discretization.calc_uvw import calc_uvw
 from scrins.discretization.corr_uvw import corr_uvw
+from scrins.discretization.nodes import nodes
 from scrins.discretization.vol_balance import vol_balance
 from scrins.display.print_time_step import print_time_step
 from scrins.operators.avg import avg

3d6ad24268f5366eb50089ae3b335764899af8d8
Mike Mueller <mmueller@python-academy.de>
2017-05-20 00:32:42 +0200

    Remove all all.py and empty __init__.py files

--- PYTHON/scrins/constants/all.py
+++
@@ -1,7 +0,0 @@
-# Constants
-from Constants.boundary_conditions    import *
-from Constants.compass                import *
-from Constants.coordinates            import *
-from Constants.gravitational_constant import *
-from Constants.solver                 import *
-from Constants.tiny_and_huge          import *
--- PYTHON/scrins/physical_models/all.py
+++
@@ -1,3 +0,0 @@
-# Operators
-from PhysicalModels.properties_for_air   import properties_for_air
-from PhysicalModels.properties_for_water import properties_for_water

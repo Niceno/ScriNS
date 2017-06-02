c616e62ffb152ec37856ed4c0a44622db05e0ac5
Niceno Bojan <bojan.niceno@psi.ch>
2017-06-02 11:38:22 +0200

    Added a line which includes "plot_tecplot" function.

--- PYTHON/Display/all.py
+++ PYTHON/Display/all.py
@@ -1,5 +1,6 @@
 # Printing functions
 from Display.plot_isolines   import plot_isolines
+from Display.plot_tecplot    import plot_tecplot
 from Display.print_array     import print_array
 from Display.print_matrix    import print_matrix
 from Display.print_time_step import print_time_step
